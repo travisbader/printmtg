7c54bacb6b306efb3b711262b721ce1be3a42d50
Travis Bader <travis@blackpanthersoftware.com>
2020-09-28 22:16:57 -0700

    Updates

---
+++ precache-manifest.d8e724db0fabe8457565dc3da4f8a594.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "049f542e43eac8f4659748ae778c2a97",
+    "url": "/index.html"
+  },
+  {
+    "revision": "3c2a038837603a0fc15f",
+    "url": "/static/css/main.8d647987.chunk.css"
+  },
+  {
+    "revision": "c3b0d341f18e7c3a1ce2",
+    "url": "/static/js/2.2b65d14c.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.2b65d14c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3c2a038837603a0fc15f",
+    "url": "/static/js/main.5a2c27e0.chunk.js"
+  },
+  {
+    "revision": "ac24deb365e997c39772",
+    "url": "/static/js/runtime-main.14356626.js"
+  }
+]);
