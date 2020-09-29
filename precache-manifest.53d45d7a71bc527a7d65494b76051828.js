d73218465bd2c0d1ec01e46f1a1d1ea8df38d7ac
Travis Bader <travis@blackpanthersoftware.com>
2020-09-28 22:01:19 -0700

    Updates

---
+++ precache-manifest.53d45d7a71bc527a7d65494b76051828.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "20f2c4bd456bab3a387c98dcb0a5ca58",
+    "url": "/printmtg/index.html"
+  },
+  {
+    "revision": "d5005a5a4020ca0f5897",
+    "url": "/printmtg/static/css/main.8d647987.chunk.css"
+  },
+  {
+    "revision": "c3b0d341f18e7c3a1ce2",
+    "url": "/printmtg/static/js/2.2b65d14c.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/printmtg/static/js/2.2b65d14c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d5005a5a4020ca0f5897",
+    "url": "/printmtg/static/js/main.eb734215.chunk.js"
+  },
+  {
+    "revision": "05e359c4d3408a9d470c",
+    "url": "/printmtg/static/js/runtime-main.06193aca.js"
+  }
+]);
